db14163352ea382a8d70dd5303e96d70c20c30bd
Muhammad Qasim Ali <muhammad.bsse4392@iiu.edu.pk>
2025-05-27 21:57:21 +0500

    Added Function PrintMonthCalendar to print
    Add Program class with month calendar printing feature
    
    Created a new `Program` class with a `Main` method and a
    `PrintMonthCalendar` method that prints the calendar for a
    given month and year. Removed previous content and added
    necessary using directives for `System` and
    `System.Globalization`.

--- FourHundredYearsCalculator/FourHundredYearsCalculator/Program.cs
+++ FourHundredYearsCalculator/FourHundredYearsCalculator/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        
+    }
+
+    static void PrintMonthCalendar(int year, int month)
+    {
+        string monthName = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(month);
+        Console.WriteLine($"\n{monthName} {year}");
+        Console.WriteLine("Su Mo Tu We Th Fr Sa");
+
+        DateTime firstDay = new DateTime(year, month, 1);
+        int startDay = (int)firstDay.DayOfWeek;
+        int daysInMonth = DateTime.DaysInMonth(year, month);
+
+        for (int i = 0; i < startDay; i++)
+        {
+            Console.Write("   ");
+        }
+
+        for (int day = 1; day <= daysInMonth; day++)
+        {
+            Console.Write($"{day,2} ");
+            if ((startDay + day) % 7 == 0)
+                Console.WriteLine();
+        }
+
+        Console.WriteLine();
+    }
+}
